2583d6a0551c4f983831c9fe192eea9c1370e6c2
funkam <alexander.funk@ukdd.de>
2023-01-25 10:48:46 +0100

    further testing

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
-#
+#test
 # Find out more about building applications with Shiny here:
 #    http://shiny.rstudio.com/
 #
